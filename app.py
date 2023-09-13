4a97cc8920eb7da8cceebada5cea1143ef1897b5
Hardy <deep.work.max@gmail.com>
2023-09-13 08:00:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import boto3
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QFileDialog,
@@ -8,7 +9,6 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QInputDialog
 from PyQt5.QtWidgets import QGridLayout
-
 
 s3 = boto3.client('s3')
 
@@ -27,6 +27,7 @@ class AutoCloseMessageBox(QMessageBox):
 class S3Client(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.uploadFolderBtn = None
         self.deleteBucketBtn = None
         self.showBucketPolicyBtn = None
         self.deleteBtn = None
@@ -71,43 +72,77 @@ class S3Client(QMainWindow):
         # 上传按钮
         self.uploadBtn = QPushButton('Upload File', self)
         self.uploadBtn.clicked.connect(self.upload_file)
-        btnGrid.addWidget(self.uploadBtn, 0, 0)  # 第一行第一列
+        # btnGrid.addWidget(self.uploadBtn, 0, 0)  # 第一行第一列
 
         # 下载按钮
         self.downloadBtn = QPushButton('Download File', self)
         self.downloadBtn.clicked.connect(self.download_file)
-        btnGrid.addWidget(self.downloadBtn, 0, 1)  # 第一行第二列
+        # btnGrid.addWidget(self.downloadBtn, 0, 1)  # 第一行第二列
 
         # 删除文件按钮
         self.deleteBtn = QPushButton('Delete File', self)
         self.deleteBtn.clicked.connect(self.delete_file)
-        btnGrid.addWidget(self.deleteBtn, 0, 2)  # 第一行第三列
+        # btnGrid.addWidget(self.deleteBtn, 0, 2)  # 第一行第三列
 
         # 预签名URL按钮
         self.generateLinkBtn = QPushButton('Generate Pre-Signed URL', self)
         self.generateLinkBtn.clicked.connect(self.generate_presigned_url)
-        btnGrid.addWidget(self.generateLinkBtn, 0, 3)  # 第一行第四列
+        # btnGrid.addWidget(self.generateLinkBtn, 0, 3)  # 第一行第四列
 
         # Show Bucket Policy Button
         self.showBucketPolicyBtn = QPushButton('Show Bucket Policy', self)
         self.showBucketPolicyBtn.clicked.connect(self.show_bucket_policy)
-        btnGrid.addWidget(self.showBucketPolicyBtn, 1, 0)  # 第二行第一列
+        # btnGrid.addWidget(self.showBucketPolicyBtn, 1, 0)  # 第二行第一列
 
         # Show Versioning Button
         self.showVersioningBtn = QPushButton('Show Versioning', self)
         self.showVersioningBtn.clicked.connect(self.show_versioning)
-        btnGrid.addWidget(self.showVersioningBtn, 1, 1)  # 第二行第二列
+        # btnGrid.addWidget(self.showVersioningBtn, 1, 1)  # 第二行第二列
 
         # Create Bucket Button
         self.createBucketBtn = QPushButton('Create New Bucket', self)
         self.createBucketBtn.clicked.connect(self.create_bucket)
-        btnGrid.addWidget(self.createBucketBtn, 1, 2)  # 第二行第三列
+        # btnGrid.addWidget(self.createBucketBtn, 1, 2)  # 第二行第三列
 
         # Delete Bucket Button
         self.deleteBucketBtn = QPushButton('Delete Selected Bucket', self)
         self.deleteBucketBtn.clicked.connect(self.delete_bucket)
-        btnGrid.addWidget(self.deleteBucketBtn, 1, 3)  # 第二行第四列
+        # btnGrid.addWidget(self.deleteBucketBtn, 1, 3)  # 第二行第四列
 
+        # 上传文件夹按钮
+        self.uploadFolderBtn = QPushButton('Upload Folder', self)
+        self.uploadFolderBtn.clicked.connect(self.upload_folder)
+        # btnGrid.addWidget(self.uploadFolderBtn, 2, 0)  # 第三行第一列
+
+        # 下载文件夹按钮
+        self.downloadFolderBtn = QPushButton('Download Folder', self)
+        self.downloadFolderBtn.clicked.connect(self.download_folder)
+        # btnGrid.addWidget(self.downloadFolderBtn, 2, 1)  # 第三行第二列
+
+        # 编辑桶策略按钮
+        self.editBucketPolicyBtn = QPushButton('Edit Bucket Policy', self)
+        self.editBucketPolicyBtn.clicked.connect(self.edit_bucket_policy)
+        # btnGrid.addWidget(self.editBucketPolicyBtn, 2, 2)  # 第三行第三列
+
+        # 开启静态网站按钮
+        self.enableStaticWebsiteBtn = QPushButton('Enable Static Website', self)
+        self.enableStaticWebsiteBtn.clicked.connect(self.enable_static_website)
+        # btnGrid.addWidget(self.enableStaticWebsiteBtn, 2, 3)  # 第三行第四列
+
+        self.emptyBucketBtn = QPushButton('Empty Bucket', self)
+        self.emptyBucketBtn.clicked.connect(self.empty_bucket)
+        # btnGrid.addWidget(self.emptyBucketBtn, 3, 0)  # 第三行第一列
+        buttons = [
+            [self.uploadBtn, self.downloadBtn, self.deleteBtn, self.generateLinkBtn],
+            [self.showBucketPolicyBtn, self.showVersioningBtn, self.createBucketBtn, self.deleteBucketBtn],
+            [self.uploadFolderBtn, self.downloadFolderBtn, self.editBucketPolicyBtn, self.enableStaticWebsiteBtn],
+            [self.emptyBucketBtn, None, None, None]  # 您的emptyBucketBtn是第四行第一列，其余位置没有按钮
+        ]
+
+        for x, row in enumerate(buttons):
+            for y, button in enumerate(row):
+                if button:  # 检查是否真的有按钮要添加
+                    btnGrid.addWidget(button, x, y)
         # 将网格布局添加到主布局
         layout.addLayout(btnGrid)
 
@@ -250,6 +285,93 @@ class S3Client(QMainWindow):
                 self.listWidget.addItem(f"Bucket {bucket_name} deleted successfully!")
             except Exception as e:
                 self.listWidget.addItem(f"Error deleting bucket: {str(e)}")
+
+    def upload_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, 'Select Folder to Upload')
+        if folder_path:
+            folder_name = os.path.basename(folder_path)  # 获取文件夹的名字
+            try:
+                for dirpath, _, filenames in os.walk(folder_path):
+                    for filename in filenames:
+                        filepath = os.path.join(dirpath, filename)
+                        relative_path = os.path.relpath(filepath, folder_path)
+                        s3_key = os.path.join(folder_name, relative_path)
+                        with open(filepath, 'rb') as file:
+                            s3.upload_fileobj(file, self.bucketComboBox.currentText(), s3_key)
+                self.list_files_in_bucket()
+            except Exception as e:
+                self.listWidget.addItem(f"Error: {str(e)}")
+
+    def download_folder_contents(self, bucket_name, prefix, local_folder):
+        try:
+            results = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+            if 'Contents' in results:
+                for item in results['Contents']:
+                    key = item['Key']
+                    if not key.endswith('/'):  # skip directories
+                        local_file_path = os.path.join(local_folder, os.path.relpath(key, prefix))
+                        local_file_dir = os.path.dirname(local_file_path)
+                        os.makedirs(local_file_dir, exist_ok=True)
+                        s3.download_file(bucket_name, key, local_file_path)
+        except Exception as e:
+            self.listWidget.addItem(f"Error: {str(e)}")
+
+    def download_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, 'Select where to save the folder contents')
+        selected_item = self.listWidget.currentItem()
+        if folder_path and selected_item:
+            self.download_folder_contents(self.bucketComboBox.currentText(), selected_item.text(), folder_path)
+
+    def edit_bucket_policy(self):
+        bucket_name = self.bucketComboBox.currentText()
+        try:
+            current_policy = s3.get_bucket_policy(Bucket=bucket_name)['Policy']
+        except:
+            current_policy = "{}"
+
+        new_policy, ok = QInputDialog.getMultiLineText(self, 'Edit Bucket Policy', 'Policy:', current_policy)
+        if ok:
+            s3.put_bucket_policy(Bucket=bucket_name, Policy=new_policy)
+
+    def enable_static_website(self):
+        bucket_name = self.bucketComboBox.currentText()
+        index_doc = 'index.html'
+        error_doc = 'error.html'
+
+        s3.put_bucket_website(
+            Bucket=bucket_name,
+            WebsiteConfiguration={
+                'ErrorDocument': {'Key': error_doc},
+                'IndexDocument': {'Suffix': index_doc},
+            }
+        )
+
+    def empty_bucket(self):
+        bucket_name = self.bucketComboBox.currentText()
+        msgbox = QMessageBox()
+        msgbox.setWindowTitle("Empty Bucket Confirmation")
+        msgbox.setIcon(QMessageBox.Warning)
+        msgbox.setText(
+            f"Are you sure you want to empty the bucket: {bucket_name}? All files and folders will be deleted!")
+        msgbox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        msgbox.setDefaultButton(QMessageBox.No)
+        reply = msgbox.exec_()
+
+        if reply == QMessageBox.Yes:
+            try:
+                # Keep deleting until all objects are gone
+                while True:
+                    objects_to_delete = s3.list_objects_v2(Bucket=bucket_name)
+                    if 'Contents' in objects_to_delete:
+                        objects = [{'Key': obj['Key']} for obj in objects_to_delete['Contents']]
+                        s3.delete_objects(Bucket=bucket_name, Delete={'Objects': objects})
+                    else:
+                        break  # No more objects to delete
+
+                self.listWidget.addItem(f"Bucket {bucket_name} emptied successfully!")
+                self.list_files_in_bucket()
+            except Exception as e:
+                self.listWidget.addItem(f"Error emptying bucket: {str(e)}")
 
 
 if __name__ == '__main__':
